3dcf392b5bd26e36719faecc7533e3a729648c7d
Facarval <facarval@student.42.fr>
2025-03-17 21:01:47 +0100

    Found password reverse_me 3

--- reverse_me/level3/source.c
+++ reverse_me/level3/source.c
@@ -3,67 +3,53 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void no()
-{
-  puts("Nope.");
-  exit(1);
+void  ___syscall_malloc() {
+  puts("Nope.")
+  exit(1)
 }
 
-int ok()
-{
-  return puts("Good job.");
+void ____syscall_malloc() {
+  puts("Good job.") 
 }
 
+int main(void) {
+  char buffer[9];
 
+  printf("Please enter key: ")
 
-int main(int argc, const char **argv, const char **envp)
-{
-  char v3; // al
-  size_t v5; // [esp+10h] [ebp-48h]
-  bool v6; // [esp+17h] [ebp-41h]
-  char nptr[4]; // [esp+1Fh] [ebp-39h] BYREF
-  char v8; // [esp+23h] [ebp-35h] BYREF
-  char v9[23]; // [esp+24h] [ebp-34h]
-  char s[9]; // [esp+3Bh] [ebp-1Dh] BYREF
-  size_t v11; // [esp+44h] [ebp-14h]
-  int i; // [esp+48h] [ebp-10h]
-  int v13; // [esp+4Ch] [ebp-Ch]
-  int v14; // [esp+50h] [ebp-8h]
+  int scanfRet = scanf("%23s", buffer)
 
-  v14 = 0;
-  printf("Please enter key: ");
-  v13 = scanf("%23s", &v8);
-  if ( v13 != 1 )
-    no();
-  if ( v9[0] != 48 )
-    no();
-  if ( v8 != 48 )
-    no();
-  fflush(stdin);
-  memset(s, 0, sizeof(s));
-  s[0] = 100;
-  nptr[3] = 0;
-  v11 = 2;
-  for ( i = 1; ; ++i )
-  {
-    v6 = 0;
-    if ( strlen(s) < 8 )
-    {
-      v5 = v11;
-      v6 = v5 < strlen(&v8);
-    }
-    if ( !v6 )
-      break;
-    nptr[0] = v9[v11 - 1];
-    nptr[1] = v9[v11];
-    nptr[2] = v9[v11 + 1];
-    v3 = atoi(nptr);
-    s[i] = v3;
-    v11 += 3;
+  if scanfRet != 1 {
+    ___syscall_malloc()
   }
-  s[i] = 0;
-  if ( strcmp(s, "delabere") )
-    no();
-  ok();
+  if  buffer[1] != '2' {
+    ___syscall_malloc()
+  }
+  if buffer[0] != '4' {
+    ___syscall_malloc()
+  }
+  fflush(stdin)
+
+  int idx = 2;
+  int pos = 0;
+  while true {
+    if (strlen("*")) {
+
+    }
+    if (strlen(buffer)) {
+
+    }
+    char atoi_buf[4];
+    atoi_buf[0] = buffer[idx];
+    atoi_buf[1] = buffer[idx++];
+    atoi_buf[2] = buffer[idx++];
+    atoi_buf[3] = 0;  
+    atoi_res = atoi(atoi_buf);
+    buffer[pos] = atoi_res
+    pos++;
+  }
+  if (strcmp("********"), buffer) {
+    ____syscall_malloc()
+  }
   return 0;
 }
