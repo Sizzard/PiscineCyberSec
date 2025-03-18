4f87b610b37149fc1094bf1e6800be724d6d43d2
Fabio Carvalho Lopes <facarval@paul-f5Ar4s3.clusters.42paris.fr>
2025-03-18 16:46:31 +0100

    e

--- reverse_me/level2/source.c
+++ reverse_me/level2/source.c
@@ -14,56 +14,54 @@ int ok()
   return puts("Good job.");
 }
 
-
-
-int main(int argc, const char **argv, const char **envp)
+int main(void)
 {
-  char v3; // al
-  size_t v5; // [esp+10h] [ebp-48h]
-  bool v6; // [esp+17h] [ebp-41h]
-  char nptr[4]; // [esp+1Fh] [ebp-39h] BYREF
-  char v8; // [esp+23h] [ebp-35h] BYREF
-  char v9[23]; // [esp+24h] [ebp-34h]
-  char s[9]; // [esp+3Bh] [ebp-1Dh] BYREF
-  size_t v11; // [esp+44h] [ebp-14h]
-  int i; // [esp+48h] [ebp-10h]
-  int v13; // [esp+4Ch] [ebp-Ch]
-  int v14; // [esp+50h] [ebp-8h]
 
-  v14 = 0;
+  char scanf_res[80];
+  char buffer[9];
+
   printf("Please enter key: ");
-  v13 = scanf("%23s", &v8);
-  if ( v13 != 1 )
+  int scanf_ret = scanf("%23s", scanf_res);
+  if (scanf_ret != 1) {
     no();
-  if ( v9[0] != 48 )
-    no();
-  if ( v8 != 48 )
-    no();
-  fflush(stdin);
-  memset(s, 0, sizeof(s));
-  s[0] = 100;
-  nptr[3] = 0;
-  v11 = 2;
-  for ( i = 1; ; ++i )
-  {
-    v6 = 0;
-    if ( strlen(s) < 8 )
-    {
-      v5 = v11;
-      v6 = v5 < strlen(&v8);
-    }
-    if ( !v6 )
-      break;
-    nptr[0] = v9[v11 - 1];
-    nptr[1] = v9[v11];
-    nptr[2] = v9[v11 + 1];
-    v3 = atoi(nptr);
-    s[i] = v3;
-    v11 += 3;
   }
-  s[i] = 0;
-  if ( strcmp(s, "delabere") )
+  if (scanf_res[0] != '0') {
     no();
-  ok();
-  return 0;
+  }
+  if (scanf_res[1] != '0') {
+    no();
+  }
+  fflush(stdin);
+  memset(buffer,0,9);
+  buffer[0] = 'd';
+  int pos = 1;
+  int i = 2;
+  char atoi_buf[4];
+  while(1) {
+    int len_1 = strlen(buffer);
+    if (len_1 >= 8) {
+      break;
+    } 
+    int scanf_len = strlen(scanf_res);
+    if (i >= scanf_len) {
+      break;
+    }
+
+    atoi_buf[0] = scanf_res[i];
+    atoi_buf[1] = scanf_res[i+1];
+    atoi_buf[2] = scanf_res[i+2];
+    atoi_buf[3] = 0;
+
+    int atoi_ret = atoi(atoi_buf);
+    buffer[pos] = atoi_ret;
+    i += 3;
+    pos++;
+  }
+
+  if (!strcmp(buffer, "delabere")) {
+    ok();
+  }
+  else {
+    no();
+  }
 }
--- reverse_me/level3/source.c
+++ reverse_me/level3/source.c
@@ -4,52 +4,89 @@
 #include <stdbool.h>
 
 void  ___syscall_malloc() {
-  puts("Nope.")
-  exit(1)
+  puts("Nope.");
+  exit(1);
 }
 
 void ____syscall_malloc() {
-  puts("Good job.") 
+  puts("Good job.");
 }
 
 int main(void) {
   char buffer[9];
+  char scanf_res[80];
 
-  printf("Please enter key: ")
+  printf("Please enter key: ");
 
-  int scanfRet = scanf("%23s", buffer)
+  int scanfRet = scanf("%23s", scanf_res);
 
-  if scanfRet != 1 {
-    ___syscall_malloc()
+  if (scanfRet != 1) {
+    ___syscall_malloc();
   }
-  if  buffer[1] != '2' {
-    ___syscall_malloc()
+  if  (scanf_res[1] != '2') {
+    ___syscall_malloc();
   }
-  if buffer[0] != '4' {
-    ___syscall_malloc()
+  if (scanf_res[0] != '4') {
+    ___syscall_malloc();
   }
-  fflush(stdin)
-
+  fflush(stdin);
+  memset(buffer,0,9);
+  buffer[0] = '*';
   int idx = 2;
   int pos = 0;
-  while true {
-    if (strlen("*")) {
-
+  int q = 1;
+  while( true ) {
+    int buff_len = strlen(buffer);
+    if (buff_len < 8) {
+      int scanf_len = strlen(scanf_res);
+      if (scanf_len > buff_len) {
+          printf("%d\n", scanf_len > buff_len);
+          break;
+      }
     }
-    if (strlen(buffer)) {
-
-    }
-    char atoi_buf[4];
-    atoi_buf[0] = buffer[idx];
-    atoi_buf[1] = buffer[idx++];
-    atoi_buf[2] = buffer[idx++];
-    atoi_buf[3] = 0;  
-    atoi_res = atoi(atoi_buf);
-    buffer[pos] = atoi_res
-    pos++;
+    char atoi_ptr[4];
+    atoi_ptr[0] = scanf_res[idx];
+    atoi_ptr[1] = scanf_res[idx + 1];
+    atoi_ptr[2] = scanf_res[idx + 2];
+    atoi_ptr[3] = 0;
+    int atoi_res = atoi(atoi_ptr);
+    buffer[pos] = (char)atoi_res;
+    printf("buff : %c ;pos : %d\n",buffer[pos], pos);
+    pos = pos + 1;
+    idx += 3;
   }
-  if (strcmp("********"), buffer) {
-    ____syscall_malloc()
+  buffer[pos] = '\0';
+  printf("%s\n", buffer);
+  int cmp_res = strcmp(buffer,"********");
+  if (cmp_res == -2) {
+    ___syscall_malloc();
+  }
+  else if (cmp_res == -1) {
+    ___syscall_malloc();
+  }
+  else if (cmp_res == 0) {
+    ____syscall_malloc();
+  }
+  else if (cmp_res == 1) {
+    ___syscall_malloc();
+  }
+  else if (cmp_res == 2) {
+    ___syscall_malloc();
+  }
+  else if (cmp_res == 3) {
+    ___syscall_malloc();
+  }
+  else if (cmp_res == 4) {
+    ___syscall_malloc();
+  }
+  else if (cmp_res == 5) {
+    ___syscall_malloc();
+  }
+  else if (cmp_res == 0x73) {
+    ___syscall_malloc();
+  }
+  else {
+    ___syscall_malloc();
   }
   return 0;
 }
